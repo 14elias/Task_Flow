a52f97061399ef65c187e0c545fd48f63ba2ed33
14elias <ellamebrahtom1995@gmail.com>
2025-11-10 11:28:58 +0300

    dependency functions added

---
+++ app/api/deps.py
@@ -0,0 +1,26 @@
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError
+from sqlalchemy.orm import Session
+from app.db.session import get_db
+from app.core.security import decode_access_token
+from app import crud, models
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/v1/auth/login")
+
+
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db) ):
+    payload = decode_access_token(token)
+    if payload is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
+
+    user = crud.crud_user.get_by_username(db, username=payload.get("sub"))
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="User not found")
+
+    return user
+
+def get_current_active_user(current_user: models.user.User = Depends(get_current_user)):
+    if not current_user.is_active:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Inactive user")
+    return current_user
