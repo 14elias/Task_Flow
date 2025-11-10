4ff78122c2380b93b64b0fef35f6f780d4d67d5c
14elias <ellamebrahtom1995@gmail.com>
2025-11-10 20:44:22 +0300

    security check added

--- app/api/deps.py
+++ app/api/deps.py
@@ -3,14 +3,15 @@ from fastapi.security import OAuth2PasswordBearer
 from jose import JWTError
 from sqlalchemy.orm import Session
 from app.db.session import get_db
-from app.core.security import decode_access_token
-from app import crud, models
+from ..core import security
+from .. import crud, models
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/v1/auth/login")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
 
 def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db) ):
-    payload = decode_access_token(token)
+    payload = security.decode_hash_token(token)
+    print(token)
     if payload is None:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
 
