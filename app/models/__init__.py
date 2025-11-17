fc66bb711d1fa1e3eaef819b2d7bff2476d7e608
14elias <ellamebrahtom1995@gmail.com>
2025-11-17 12:48:42 +0300

    db update

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,1 +1,45 @@
-from .user import User
+from .base import Base
+
+# User & Role
+from .user import User, RoleEnum
+
+# Team Models
+from .team import Team, TeamMember
+
+# Project Models
+from .project import Project, ProjectTeam
+
+# Task Models
+from .task import Task, TaskStatus
+
+# Comment Model
+from .comment import Comment
+
+# Notification Model
+from .notification import Notification
+
+__all__ = [
+    "Base",
+
+    # user
+    "User",
+    "RoleEnum",
+
+    # team
+    "Team",
+    "TeamMember",
+
+    # project
+    "Project",
+    "ProjectTeam",
+
+    # task
+    "Task",
+    "TaskStatus",
+
+    # comment
+    "Comment",
+
+    # notification
+    "Notification",
+]
--- app/models/user.py
+++ app/models/user.py
@@ -1,12 +1,33 @@
-from sqlalchemy import Column, Integer, String, Boolean
-from app.db.session import Base
+from sqlalchemy import String, Boolean, Enum, DateTime
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from datetime import datetime
+from enum import Enum as PyEnum
+
+from .base import Base
+
+
+class RoleEnum(PyEnum):
+    ADMIN = "admin"
+    MANAGER = "manager"
+    MEMBER = "member"
+    CLIENT = "client"
+
 
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, index=True)
-    email = Column(String, unique=True, index=True)
-    hashed_password = Column(String)
-    is_active = Column(Boolean, default=True)
-    role = Column(String, default = 'user')
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    full_name: Mapped[str] = mapped_column(String(100))
+    email: Mapped[str] = mapped_column(String(150), unique=True, index=True)
+    hashed_password: Mapped[str] = mapped_column(String(255))
+    role: Mapped[RoleEnum] = mapped_column(Enum(RoleEnum), default=RoleEnum.MEMBER)
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True)
+
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow)
+
+    # Relationships
+    teams: Mapped[list["TeamMember"]] = relationship(back_populates="user")
+    assigned_tasks: Mapped[list["Task"]] = relationship(back_populates="assignee")
+    comments: Mapped[list["Comment"]] = relationship(back_populates="user")
+    notifications: Mapped[list["Notification"]] = relationship(back_populates="user")
+    managed_projects: Mapped[list["Project"]] = relationship(back_populates="manager")
