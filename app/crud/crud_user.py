8f4de41a5e672f85ad429ae6704277cc57a1d016
14elias <ellamebrahtom1995@gmail.com>
2025-11-27 23:56:27 +0300

    refctoring the user endpoint

--- app/crud/crud_user.py
+++ app/crud/crud_user.py
@@ -28,3 +28,67 @@ def delete_user(username, db):
         raise HTTPException(status_code=404, detail="User not found")
     db.delete(user)
     db.commit()
+
+    return user
+
+
+def update_user(db, current_user, data):
+    user = db.query(User).filter(User.username == current_user.username).first()
+
+    if not user:
+        raise HTTPException(status_code=404, detail='user not found')
+
+    user.email = data.email
+    user.username = data.username
+
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+
+    return user
+
+
+def get_all_user(db):
+    users = db.query(User).all()
+
+    if not users:
+        raise HTTPException(status_code=404, detail='there are not users')
+    
+    return users
+
+
+def deactivate(db, username):
+    user = db.query(User).filter(User.username == username).first()
+
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    
+    user.is_active = False
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+
+    return user
+
+
+def activate(db, username):
+    user = db.query(User).filter(User.username == username).first()
+
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    
+    user.is_active = True
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+
+    return user
+
+
+def get_user_tasks(db, id):
+    user = db.query(User).filter(User.id == id).first()
+
+    if not user:
+        raise HTTPException(status_code=404, detail='user not exist')
+    
+    return user.assigned_tasks
