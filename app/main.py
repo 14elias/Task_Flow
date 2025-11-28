f99a50dd3bfc201c00bab15539727586e6b9d738
14elias <ellamebrahtom1995@gmail.com>
2025-11-28 23:49:28 +0300

    refactoring the main file

--- app/main.py
+++ app/main.py
@@ -1,5 +1,12 @@
 from fastapi import FastAPI
-from app.api.v1 import user_routes, auth_routes, team_routes, project_routes, task_ruotes
+from app.api.v1 import (
+    user_routes, 
+    auth_routes, 
+    team_routes, 
+    project_routes,
+    task_ruotes,
+    comment_routes
+)
 from .db.init_db import init_db
 # from .db.session import Base, engine
 
@@ -9,6 +16,7 @@ app.include_router(auth_routes.router, tags=["auth"])
 app.include_router(team_routes.router, tags=["team"])
 app.include_router(project_routes.router, tags=["project"])
 app.include_router(task_ruotes.router, tags=["task"])
+app.include_router(comment_routes.router, tags=["comment"])
 
 @app.on_event("startup")
 def start():
